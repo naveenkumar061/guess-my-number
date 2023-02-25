51983aabad76d44f1f77a895cc3e0b0cd2e45c1f
D NAVEEN KUMAR <devarashettynaveen224@gmail.com>
2023-02-25 12:13:47 +0530

    manipulateCSS commit

--- script.js
+++ script.js
@@ -12,23 +12,33 @@ console.log(document.querySelector('.guess').value);
 
 const secretNumber = Math.trunc(Math.random() * 20);
 let score = 20;
-document.querySelector('.number').textContent = secretNumber;
 
 document.querySelector('.check').addEventListener('click', function () {
   const guess = Number(document.querySelector('.guess').value);
   console.log(guess, typeof guess);
+  //NO INPUT
   if (!guess) {
     document.querySelector('.message').textContent = 'No Number!';
-  } else if (guess === secretNumber) {
+  }
+  //WINS
+  else if (guess === secretNumber) {
     document.querySelector('.message').textContent = 'Correct!';
-  } else if (guess > secretNumber) {
+    document.querySelector('body').style.backgroundColor = '#60b347';
+    document.querySelector('.number').style.width = '30rem';
+    document.querySelector('.guess').style.backgroundColor = '#60b347';
+    document.querySelector('.number').textContent = secretNumber;
+  }
+  //TOO HIGH
+  else if (guess > secretNumber) {
     if (score > 1) {
       document.querySelector('.message').textContent = 'Too High!';
     } else {
       document.querySelector('.message').textContent = 'You Lost Game!';
     }
     score--;
-  } else if (guess < secretNumber) {
+  }
+  //TOO LOW
+  else if (guess < secretNumber) {
     if (score > 1) {
       document.querySelector('.message').textContent = 'Too Low!';
     } else {
@@ -37,4 +47,15 @@ document.querySelector('.check').addEventListener('click', function () {
     score--;
   }
   document.querySelector('.score').textContent = score;
+});
+
+document.querySelector('.again').addEventListener('click', function () {
+  score = 20;
+  document.querySelector('.message').textContent = 'Start guessing...';
+  document.querySelector('.score').textContent = score;
+  document.querySelector('.number').textContent = '?';
+  document.querySelector('.guess').value = '';
+  document.querySelector('body').style.backgroundColor = '#222';
+  document.querySelector('.guess').style.backgroundColor = '#222';
+  document.querySelector('.number').style.width = '15rem';
 });
