b13afea9ab686f9eda0494c39096a29ccf9ba38b
D NAVEEN KUMAR <devarashettynaveen224@gmail.com>
2023-02-25 12:29:01 +0530

    implementHigh commit

--- script.js
+++ script.js
@@ -12,6 +12,7 @@ console.log(document.querySelector('.guess').value);
 
 const secretNumber = Math.trunc(Math.random() * 20);
 let score = 20;
+let highScore = 0;
 
 document.querySelector('.check').addEventListener('click', function () {
   const guess = Number(document.querySelector('.guess').value);
@@ -27,6 +28,10 @@ document.querySelector('.check').addEventListener('click', function () {
     document.querySelector('.number').style.width = '30rem';
     document.querySelector('.guess').style.backgroundColor = '#60b347';
     document.querySelector('.number').textContent = secretNumber;
+    if (score > highScore) {
+      highScore = score;
+      document.querySelector('.highscore').textContent = highScore;
+    }
   }
   //TOO HIGH
   else if (guess > secretNumber) {
